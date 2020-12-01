e9b95b537df95727be37c152ef290c1d4037fef0
Davidresume <davidresume@hotmail.com>
2020-12-01 09:45:30 -0500

    Tabs layout setup

---
+++ app/src/main/java/com/example/instagramclone/ProfileTab.java
@@ -0,0 +1,44 @@
+package com.example.instagramclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileTab#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileTab extends Fragment {
+
+
+
+    public ProfileTab() {
+        // Required empty public constructor
+    }
+
+    public static ProfileTab newInstance(String param1, String param2) {
+        ProfileTab fragment = new ProfileTab();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+    }
+}
--- app/src/main/java/com/example/instagramclone/SociaMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SociaMediaActivity.java
@@ -1,14 +1,37 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SociaMediaActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private  TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socia_media);
+
+        setTitle("Social Media App!!!");
+        toolbar = findViewById(R.id.myToolbar);
+
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(),1);
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
+
+
+
     }
 }
